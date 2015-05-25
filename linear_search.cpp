dffb31a8d22356b18d03e748e03d7a4bcb47028d
Michael Drummond <michael.drummond@my.liu.edu>
2015-05-25 13:28:36 -0400

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -2,42 +2,41 @@
 #include <iostream>
 using namespace std;
 
+int position = -1;
+
 int linear_search(char Array[],int size,char search)
 {
-    for(int i=0; i <size; i++)
-    {
-        if (Array[i]==search)
-        {
-            return i;
-        }
-
-    }
-    return -1;
+	for(position =0; position <size; position++)
+	{
+		if (Array[position]==search)
+		{
+		return position;
+		}
+	}
+	return position;
 }
 
 int main()
 {
-
-    const int size = 100;
-    char Array[size];
-    char search;
-
-    cout <<"Enter characters ("<< size <<") max:";
-    cin.getline (Array,size);
-
-    cout<<"\nEnter search:";
-    cin >> search;
-
-    int result= linear_search(Array,size,search);
-
-    if(result == -1)
-    {
-        cout <<"\n"<<"Not found";
-    }
-    else
-    {
-        cout << "\n" << search <<" found at location: " << result << endl;
-    }
-
-    return 0;
+	const int size = 100;
+	char Array[size];
+	char search;
+	
+	cout <<"Enter characters ("<< size <<") max:";
+	cin.getline (Array,size);
+	
+	cout<<"\nEnter search:";
+	cin >> search;
+	
+	int result= linear_search(Array,size,search);
+	
+	if(result == -1)
+	{
+		cout <<"\n"<<search<<" not found";
+	}
+	else
+	{
+		cout << "\n" << search <<" found at location: " << position << endl;
+	}
+	return 0;
 }
